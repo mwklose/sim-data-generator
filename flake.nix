903004bdd099f5c9bcda01f140265ab0c56e61d1
mwklose <markklose17@gmail.com>
2024-10-24 13:07:15 -0400

    feat: nix flake init

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Python app with Qt GUI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";  # Using nixos-unstable for up-to-date packages
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs { inherit system; };
+
+    # Python environment with PyQt5 (Qt for Python)
+    pythonEnv = pkgs.python312.withPackages (ps: with ps; [
+      pyqt5       # PyQt5 for Qt support
+      polars      # polars
+      numpy
+      statsmodels 
+      networkx
+      matplotlib
+    ]);
+
+  in {
+    # Defining the default package, built using the Python environment
+    packages.default = pkgs.mkShell {
+      buildInputs = [ pythonEnv ];  # Adding the Python environment to the build inputs
+    };
+
+    # DevShell allows interactive development
+    devShells.default = pkgs.mkShell {
+      buildInputs = [ pythonEnv ];
+    };
+
+    # A NixOS module for adding the app to a NixOS configuration
+    nixosModules.default = {
+      config, pkgs, ... }: {
+        environment.systemPackages = [ pythonEnv ];
+      };
+  });
+}
